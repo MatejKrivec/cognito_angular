da512c758c8ee16e8f591422c17eb6c70e9d009b
MatejKrivec <matej.krivec@gmail.com>
2024-08-26 22:23:01 +0200

    lokalizacija in template email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import * as Auth from '@aws-amplify/auth';
-import { getCurrentUser, fetchAuthSession } from 'aws-amplify/auth';
+import { Component, OnInit, ChangeDetectorRef, NgZone } from '@angular/core';
+import { I18n } from 'aws-amplify/utils';
+import { translations } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cognito-angular';
+  currentLanguage = 'fr'; // Default language
 
   formFields = {
     signUp: {
@@ -27,5 +28,54 @@ export class AppComponent {
     },
   };
 
-  
+  constructor(private cdr: ChangeDetectorRef, private ngZone: NgZone) {}
+
+  ngOnInit() {
+    // Read the language from local storage if available
+    const savedLanguage = localStorage.getItem('language');
+    if (savedLanguage) {
+      this.currentLanguage = savedLanguage;
+    }
+
+    // Add default translations
+    I18n.putVocabularies(translations);
+
+    // Add custom translations
+    const customTranslations = {
+      fr: {
+        'Sign In': 'Connexion',
+        'Sign Up': 'Inscription',
+        'Enter your email': 'Entrez votre email',
+        'Enter your Username': 'Entrez votre username',
+        'Password': 'Mot de passe',
+        'Forgot your password?': 'Mot de passe oublié?',
+      },
+      en: {
+        'Sign In': 'Sign In',
+        'Sign Up': 'Sign Up',
+        'Enter your email': 'Enter your email',
+        'Password': 'Password',
+        'Forgot your password?': 'Forgot your password?',
+      }
+    };
+
+    I18n.putVocabularies(customTranslations);
+    I18n.setLanguage(this.currentLanguage); // Set language based on saved preference
+
+    console.log('Initial language set to:', I18n.get(''));
+  }
+
+  onLanguageChange(event: any) {
+    const selectedLanguage = event.target.value;
+    this.currentLanguage = selectedLanguage;
+    I18n.setLanguage(selectedLanguage); // Set the language based on user selection
+
+    // Save the selected language to local storage
+    localStorage.setItem('language', selectedLanguage);
+
+    // Run change detection within Angular's zone
+    window.location.reload();
+
+    console.log(`Language changed to: ${selectedLanguage}`);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule,  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
@@ -27,6 +27,7 @@ Amplify.configure({
     FormsModule, 
     AmplifyAuthenticatorModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
